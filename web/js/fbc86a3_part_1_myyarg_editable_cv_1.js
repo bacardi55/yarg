d0ec752c3af9e1068d0a5d06219a742c8893c3c3
Raphael Khaiat <raphael@khaiat.org>
2013-11-17 23:51:03 +0100

    Add web files

---
+++ web/js/fbc86a3_part_1_myyarg_editable_cv_1.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  if ($('#yargAddModal').length) {
+    $('[data-toggle="modal"]').on('click', function (a,b,c) {
+      if ($(this).attr('data-title')) {
+        $('#yargAddModal .modal-header h3').html($(this).attr('data-title'));
+      }
+    });
+
+    $('#yargAddModal button.save').on('click', function() {
+      $('#yargAddModal').find('form').submit();
+    });
+
+    $('#yargAddModal').on('hidden', function() {
+      $(this).data('modal', null);
+    });
+
+    $('#yargAddModal').on('show', function(a,b,c) {
+      console.log(a);
+      console.log(b);
+      console.log(c);
+
+    });
+  }
+
+  if ($('#Cv_title').length) {
+    $('#Cv_title').tooltip({
+      placement: 'right',
+      title: translations.title,
+      trigger: 'hover focus',
+    });
+  }
+
+  if ($('#Cv_description').length) {
+    $('#Cv_description').tooltip({
+      placement: 'right',
+      title: translations.description,
+      trigger: 'hover focus',
+    });
+  }
+
+  if ($('#Cv_catch').length) {
+    $('#Cv_catch').tooltip({
+      placement: 'right',
+      title: translations.catch,
+      trigger: 'hover focus',
+    });
+  }
+});
